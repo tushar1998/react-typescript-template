95d973312f0ab215a2c4696697a69ac119e16b02
Tushar Mistry <mistry.tushar98@gmail.com>
2021-05-09 19:07:55 +0530

    💅 Front Page Styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { FC } from 'react';
-import GlobalStyle from './styles/globalStyles';
-import Counter from './components/Counter';
+import { Container, Header, Image, Link } from 'styles/App.style';
+import logo from 'assets/png/logo512.png';
+import GlobalStyle from 'styles/globalStyles';
 
 const App: FC = () => (
   <div>
     <GlobalStyle />
-    <h1>React TypeScript Starter Template </h1>
-    <Counter />
+    <Container>
+      <Header>
+        <h2>React TypeScript Starter Template</h2>
+        <Image src={logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </Link>
+      </Header>
+    </Container>
   </div>
 );
 
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    margin:0;
     font-family: "Nunito";
   }
   a {
