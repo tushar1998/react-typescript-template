8f1b49d3fc032350833257c34f506102b1c00f05
kevalmalde <keval.malde@raweng.com>
2021-05-15 17:35:31 +0530

    🔧 Deployment Specific Env.
    Checked against existing env.example schema

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,24 +1,5 @@
-/* eslint-disable no-console */
-const dotenv = require('dotenv');
-const path = require('path');
-const { DefinePlugin } = require('webpack');
-
-let envKeys = {};
-try {
-  const env = dotenv.config({ path: path.join(__dirname, '..', '.env.production') }).parsed;
-  envKeys = Object.keys(env).reduce((prev, next) => {
-    // eslint-disable-next-line no-param-reassign
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
-  console.log('Loaded environment variable from .env.production');
-} catch (error) {
-  console.log('.env.production file Not Found');
-}
-
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
   stats: 'normal',
-  plugins: [new DefinePlugin(envKeys)],
 };
